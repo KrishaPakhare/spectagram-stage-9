aef7084a1e5ac00fdc95b25978b57d239a0f395b
KrishaPakhare <75171593+KrishaPakhare@users.noreply.github.com>
2023-10-10 14:14:48 +0530

    Add files via upload

--- README.md
+++ README.md
@@ -1,1 +1,2 @@
-# spectagram-stage-9
+# Spectogram-stage-5
+project solution for c85
---
+++ config.js
@@ -0,0 +1,10 @@
+
+const firebaseConfig = {
+  apiKey: "AIzaSyA_CbFKAOTdFU6X8gOUxW6nu8MLnzdfl3w",
+  authDomain: "spectagram-46e6d.firebaseapp.com",
+  projectId: "spectagram-46e6d",
+  storageBucket: "spectagram-46e6d.appspot.com",
+  messagingSenderId: "1004492408194",
+  appId: "1:1004492408194:web:08d21be4e281e21c5afb9b"
+};
+
